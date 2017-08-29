c02b56056d933b17cfbb6ffd0847234103342b3a
Peter Jang <peter.x.jang@gmail.com>
2017-08-28 20:58:53 -0500

    Display array of notes

--- js/notes.js
+++ js/notes.js
@@ -1,1 +1,16 @@
 /* global $ */
+var notes = [
+  {id: 1, body: "This is a first test", timestamp: Date.now()},
+  {id: 2, body: "This is a second test", timestamp: Date.now()},
+  {id: 3, body: "This is a third test", timestamp: Date.now()}
+];
+
+notes.forEach(function(note) {
+  console.log(note);
+  $('.note-selectors').append(
+    '<div class="note-selector">' +
+      '<p class="note-selector-title">' + note.body + '</p>' +
+      '<p class="note-selector-timestamp">' + note.timestamp + '</p>' +
+    '</div>'
+  );
+});
