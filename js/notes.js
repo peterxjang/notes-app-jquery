696fabbf2d7ee571cafbb29b89d89e0338fb9a9b
Peter Jang <peter.x.jang@gmail.com>
2017-08-28 21:02:34 -0500

    Use methods to sort and format notes

--- js/notes.js
+++ js/notes.js
@@ -1,16 +1,37 @@
 /* global $ */
+function transformNotes(notes) {
+  return notes.slice().sort(function(a, b) {
+    return b.timestamp - a.timestamp;
+  });
+}
+
+function formatTitle(body) {
+  var maxLength = 20;
+  if (body.length > maxLength) {
+    return body.substring(0, maxLength - 3) + '...';
+  } else if (body.length === 0) {
+    return "New note";
+  } else {
+    return body;
+  }
+}
+
+function formatTimestamp(timestamp) {
+  return new Date(timestamp).toUTCString();
+}
+
 var notes = [
   {id: 1, body: "This is a first test", timestamp: Date.now()},
   {id: 2, body: "This is a second test", timestamp: Date.now()},
   {id: 3, body: "This is a third test", timestamp: Date.now()}
 ];
 
-notes.forEach(function(note) {
+transformNotes(notes).forEach(function(note) {
   console.log(note);
   $('.note-selectors').append(
     '<div class="note-selector">' +
-      '<p class="note-selector-title">' + note.body + '</p>' +
-      '<p class="note-selector-timestamp">' + note.timestamp + '</p>' +
+      '<p class="note-selector-title">' + formatTitle(note.body) + '</p>' +
+      '<p class="note-selector-timestamp">' + formatTimestamp(note.timestamp) + '</p>' +
     '</div>'
   );
 });
